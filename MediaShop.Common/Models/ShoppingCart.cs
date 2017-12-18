6764888a5522a7b78b31d911b0d3ff063b1b49c5
Alieksandr Kononenko <kv210@rambler.ru>
2017-12-19 02:14:49 +0300

    + class ShoppingCart (dont compile) + class SubjectCollection (dont compile)

---
+++ MediaShop.Common/Models/ShoppingCart.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaShop.Common.Models
+{
+    /// <summary>
+    /// Сlass describes ShoppingCart
+    /// </summary>
+    public class ShoppingCart
+    {
+        private string subjectCollectionId;
+
+        private List<SubjectCollection> subjectCollection;
+
+        private decimal priceAllItemsCollection;
+
+        private int countItemsInCollection;
+
+        public string SubjectCollectionId { get; set; }
+
+        /// <summary>
+        /// Collection items
+        /// </summary>
+        public List<SubjectCollection> SubjectCollection { get; set; }
+
+        /// <summary>
+        /// Property to determine price all items in collection
+        /// </summary>
+        public decimal PriceAllItemsCollection { get; set; }
+
+        /// <summary>
+        /// Property to determine the amount of content in a collection
+        /// </summary>
+        public int CountItemsInCollection { get; set; }
+
+        /// <summary>
+        /// The property to determine whether the collection is selected
+        /// </summary>
+        public bool IsChecked { get; set; }
+    }
+}
---
+++ MediaShop.Common/Models/SubjectCollection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaShop.Common.Models
+{
+    /// <summary>
+    /// Сlass describes Collection items
+    /// </summary>
+    public class SubjectCollection
+    {
+        private int contentId;
+
+        private string descriptionItem;
+
+        private decimal priceItem;
+
+        /// <summary>
+        /// Id content
+        /// </summary>
+        public int ContentId { get; set; }
+
+        /// <summary>
+        /// Description media content
+        /// </summary>
+        public string DescriptionItem { get; set; }
+
+        /// <summary>
+        /// Price media content
+        /// </summary>
+        public decimal PriceItem { get; set; }
+
+        /// <summary>
+        /// The property to determine whether the content is selected
+        /// </summary>
+        public bool IsChecked { get; set; }
+    }
+}
