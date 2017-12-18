9eb4e9be130ed98ac5e9d4d2fafce0b479fe885e
Aleksander Shaduro <aleksander_shaduro@epam.com>
2017-12-18 20:54:04 +0300

    + added base repository +added base entity

---
+++ MediaShop.Common/Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MediaShop.Common.Models
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int CreatorId { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public int? ModifierId { get; set; }
+    }
+}
