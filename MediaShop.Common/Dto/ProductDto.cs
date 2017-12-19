4cc088555552a35ee4e21af1289626c9ce6a5af1
Alexey Maloed <lyoha.live@gmail.com>
2017-12-19 23:08:05 +0300

    +added dto ProductDto +added mappers for ProductDto

---
+++ MediaShop.Common/Dto/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaShop.Common.Dto
+{
+    public class ProductDto
+    {
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
--- MediaShop.Common/MapperProfile.cs
+++ MediaShop.Common/MapperProfile.cs
@@ -6,6 +6,11 @@ namespace MediaShop.Common
     {
         public MapperProfile()
         {
+            Mapper.Initialize(config => config.CreateMap<Models.Entity, Dto.ProductDto>().ForMember(x => x.ProductId, opt => opt.MapFrom(source => source.Id)).ReverseMap());
+
+            Mapper.Initialize(config => config.CreateMap<Models.Content.Product, Dto.ProductDto>().ReverseMap());
+
+            //to do: output result, add source references (objects)
         }
     }
 }
