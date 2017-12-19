b97e05fb1d6735583544bed4807dc74620504b02
Pawel Petrusewitch <shuster.pashka@gmail.com>
2017-12-19 21:53:06 +0300

    +add class Product

---
+++ MediaShop.Common/Models/Content/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaShop.Common.Models.Content
+{
+    public class Product : Entity
+    {
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public int ProductType { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public bool IsPremium { get; set; }
+
+        public bool IsFavorite { get; set; }
+    }
+}
