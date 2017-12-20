2d7ff8a0f5d878c7580f53116b671822c15fdb3d
Irina Bey <irinabey88@gmail.com>
2017-12-20 22:33:30 +0300

    * modiffied class Account

--- MediaShop.Common/Models/User/Account.cs
+++ MediaShop.Common/Models/User/Account.cs
@@ -1,7 +1,34 @@
-﻿namespace MediaShop.Common.Models.User
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediaShop.Common.Models.User
 {
-    public class Account
+    public class Account : Entity
     {
-        
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int CreatorId { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public int? ModifierId { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Describing personal  users data
+        /// </summary>
+        public Profile AccountProfile { get; set; }
+
+        /// <summary>
+        /// Permissions  describes  list of roles, that has this user 
+        /// </summary>
+        public SortedSet<Role> Permissions { get; set; }
     }
 }
