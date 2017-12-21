0b8d69f6d0b5b1dea0d530c3b5d9ece87041d413
Pavel Ivanenko <pabl1989@mail.ru>
2017-12-21 21:04:48 +0300

    + added new folder Cart *  model Cart is moved + added new metods to ICartService

--- MediaShop.Common/Interfaces/Services/ICartService.cs
+++ MediaShop.Common/Interfaces/Services/ICartService.cs
@@ -1,13 +1,14 @@
 ﻿namespace MediaShop.Common.Interfaces.Services
 {
-    using Models;
     using System;
     using System.Collections.Generic;
+    using Models;
 
     /// <summary>
     /// Interface describing the methods of
     /// interaction with the service when working with the Cart
     /// </summary>
+    /// <typeparam name="TModel"></typeparam>
     public interface ICartService<TModel>
         where TModel : Models.Entity
     {
@@ -17,5 +18,40 @@
         /// <param name="id">user Id</param>
         /// <returns> shopping cart for a user </returns>
         IEnumerable<TModel> GetItemsInCart(int id);
+
+        /// <summary>
+        /// Get created Cart model object
+        /// </summary>
+        /// <param name="id">user Id</param>
+        /// <returns>Cart</returns>
+        Models.Cart GetCart(int id);
+
+        /// <summary>
+        /// Get sum price items for User
+        /// </summary>
+        /// <param name="id">user Id</param>
+        /// <returns>Sum price</returns>
+        float GetPrice(int id);
+
+        /// <summary>
+        /// Get sum price items typeof ContentCart
+        /// </summary>
+        /// <param name="cart">Collection ContentCart</param>
+        /// <returns>Sum price</returns>
+        float GetPrice(IEnumerable<TModel> cart);
+
+        /// <summary>
+        /// Get count items for User
+        /// </summary>
+        /// <param name="id">user Id</param>
+        /// <returns>Count Items in cart</returns>
+        uint GetCoutItems(int id);
+
+        /// <summary>
+        /// Get count items
+        /// </summary>
+        /// <param name="cart">Collection ContentCart</param>
+        /// <returns>Count Items</returns>
+        uint GetCoutItems(IEnumerable<TModel> cart);
     }
 }
