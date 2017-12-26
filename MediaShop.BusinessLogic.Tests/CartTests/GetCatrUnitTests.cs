4306d842c5be1565247713df1ddca517107e7b67
Pavel_Ivanenko <pabl1989@mail.ru>
2017-12-26 20:56:24 +0300

    + added tests to GetCartUnitTests

--- MediaShop.BusinessLogic.Tests/CartTests/GetCatrUnitTests.cs
+++ MediaShop.BusinessLogic.Tests/CartTests/GetCatrUnitTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using MediaShop.Common.Interfaces.Repositories;
@@ -34,19 +32,87 @@ namespace MediaShop.BusinessLogic.Tests.CartTests
         [TestMethod]
         public void Get_Cart()
         {
-            
+            var collectionItem = new Collection<ContentCartDto>()
+            {
+                new ContentCartDto { Id = 1, CreatorId = 1 , PriceItem = new decimal (9.99) },
+                new ContentCartDto { Id = 2, CreatorId = 1 , PriceItem = new decimal (0.50) },
+                new ContentCartDto { Id = 3, CreatorId = 1 , PriceItem = new decimal (1.01) }
+            };
+            mock.Setup(item => item.Find(It.IsAny<Expression<Func<ContentCartDto, bool>>>()))
+                .Returns(() => collectionItem);
+            var service = new CartService(mock.Object);
+            var cart = service.GetCart(1);
+            Assert.AreEqual((uint)3, cart.CountItemsInCollection);
+            Assert.AreEqual(new decimal(11.50), cart.PriceAllItemsCollection);
+            Assert.IsNotNull(cart.ContentCartCollection);
+        }
+
+        [TestMethod]
+        public void Get_EmptyCart()
+        {
+            var collectionItem = new Collection<ContentCartDto>();
+            mock.Setup(item => item.Find(It.IsAny<Expression<Func<ContentCartDto, bool>>>()))
+                .Returns(() => collectionItem);
+            var service = new CartService(mock.Object);
+            var cart = service.GetCart(1);
+            Assert.AreEqual((uint)0, cart.CountItemsInCollection);
+            Assert.AreEqual(new decimal(0), cart.PriceAllItemsCollection);
+            Assert.IsNotNull(cart.ContentCartCollection);
         }
 
         [TestMethod]
         public void Get_Price()
         {
-            
+            var collectionItem = new Collection<ContentCartDto>()
+            {
+                new ContentCartDto { Id = 1, CreatorId = 1 , PriceItem = new decimal (9.99) },
+                new ContentCartDto { Id = 2, CreatorId = 1 , PriceItem = new decimal (0.50) },
+                new ContentCartDto { Id = 3, CreatorId = 1 , PriceItem = new decimal (1.01) }
+            };
+            mock.Setup(item => item.Find(It.IsAny<Expression<Func<ContentCartDto, bool>>>()))
+                .Returns(() => collectionItem);        
+            var service = new CartService(mock.Object);
+            var price = service.GetPrice(5);
+            Assert.AreEqual(new decimal(11.50),price);           
+        }
+
+        [TestMethod]
+        public void Get_Price_emptyCart()
+        {
+            var collectionItem = new Collection<ContentCartDto>();
+            mock.Setup(item => item.Find(It.IsAny<Expression<Func<ContentCartDto, bool>>>()))
+                .Returns(() => collectionItem);
+            var service = new CartService(mock.Object);
+            var price = service.GetPrice(5);
+            Assert.AreEqual(0, price);
         }
 
         [TestMethod]
         public void Get_Count_Items_in_Cart()
         {
-          
-        }     
+            var collectionItem = new Collection<ContentCartDto>()
+            {
+                new ContentCartDto { Id = 1, CreatorId = 1 , PriceItem = new decimal (9.99) },
+                new ContentCartDto { Id = 2, CreatorId = 1 , PriceItem = new decimal (0.50) },
+                new ContentCartDto { Id = 3, CreatorId = 1 , PriceItem = new decimal (1.01) }
+            };
+            mock.Setup(item => item.Find(It.IsAny<Expression<Func<ContentCartDto, bool>>>()))
+                .Returns(() => collectionItem);
+            var service = new CartService(mock.Object);
+            var count = service.GetCountItems(5);
+            Assert.AreEqual((uint)3, count);
+        }
+
+        [TestMethod]
+        public void Get_Count_Items_in_emptyCart()
+        {
+            var collectionItem = new Collection<ContentCartDto>();
+            mock.Setup(item => item.Find(It.IsAny<Expression<Func<ContentCartDto, bool>>>()))
+                .Returns(() => collectionItem);
+            var service = new CartService(mock.Object);
+            var count = service.GetCountItems(5);
+            Assert.AreEqual((uint)0, count);
+        }
+
     }
 }
--- MediaShop.DataAccess/NInjectProfile.cs
+++ MediaShop.DataAccess/NInjectProfile.cs
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-﻿namespace MediaShop.DataAccess
-=======
-﻿using MediaShop.DataAccess.Context;
+using MediaShop.DataAccess.Context;
 using Ninject.Modules;
 
 namespace MediaShop.DataAccess
->>>>>>> develop
 {
-    using MediaShop.Common.Interfaces.Repositories;
-    using MediaShop.Common.Models;
-    using MediaShop.DataAccess.Repositories;
-    using Ninject.Modules;
-
     /// <summary>
     /// Profile Ninject container
     /// </summary>
@@ -20,11 +11,7 @@ namespace MediaShop.DataAccess
         /// <inheritdoc/>
         public override void Load()
         {
-<<<<<<< HEAD
-            this.Bind<ICartRespository<ContentCartDto>>().To<CartRepository>();
-=======
             Bind<MediaContext>().ToSelf();
->>>>>>> develop
         }
     }
 }
