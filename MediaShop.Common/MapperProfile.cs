69288c7fcd79f6bcf366b51b499cd9312d02dabb
Irina Bey <irinabey88@gmail.com>
2018-01-04 22:45:26 +0300

    * use default mapping

--- MediaShop.Common/MapperProfile.cs
+++ MediaShop.Common/MapperProfile.cs
@@ -20,14 +20,7 @@ namespace MediaShop.Common
         /// </summary>
         public MapperProfile()
         {
-            this.CreateMap<UserDto, Account>()
-                .ForMember(x => x.Id, opt => opt.MapFrom(m => m.Id))
-                .ForMember(x => x.Login, opt => opt.MapFrom(m => m.Login))
-                .ForMember(x => x.Password, opt => opt.MapFrom(m => m.Password))
-                .ForMember(x => x.CreatorId, opt => opt.MapFrom(m => m.CreatorId))
-                .ForMember(x => x.CreatedDate, opt => opt.MapFrom(m => m.CreatedDate))
-                .ForMember(x => x.ModifierId, opt => opt.MapFrom(m => m.ModifierId))
-                .ForMember(x => x.ModifiedDate, opt => opt.MapFrom(m => m.ModifiedDate)).ReverseMap();
+            this.CreateMap<UserDto, Account>().ReverseMap();
         }
     }
 }
