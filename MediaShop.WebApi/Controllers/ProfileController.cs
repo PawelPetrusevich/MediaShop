94090a2bb12c19073a7812e4143e53c444a14ade
Irina Bey <irinabey88@gmail.com>
2018-01-06 00:49:53 +0300

    + added AccountController, ProfileController, SettingsController

---
+++ MediaShop.WebApi/Controllers/ProfileController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MediaShop.Common.Models.User;
+
+namespace MediaShop.WebApi.Controllers
+{
+    [RoutePrefix("api/profile")]
+    public class ProfileController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return this.Ok(new List<AccountProfile>());
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public IHttpActionResult GetById(ulong id)
+        {
+            return this.Ok(new AccountProfile());
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] AccountProfile profile)
+        {
+            return this.Content<AccountProfile>(HttpStatusCode.Created, new AccountProfile());
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put([FromBody]AccountProfile profile)
+        {
+            return this.Ok(new AccountProfile());
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete([FromBody]ulong? id)
+        {
+            return this.Ok();
+        }
+    }
+}
