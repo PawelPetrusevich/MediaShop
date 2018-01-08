3fdba8ea282518728bc9e17cee378720c083e47a
Irina Bey <irinabey88@gmail.com>
2018-01-08 21:42:44 +0300

    + added ISettingsService, SettingsServise

---
+++ MediaShop.Common/Dto/User/ProfileDto.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="UserDto.cs" company="MediaShop">
+// Copyright (c) MediaShop. All rights reserved.
+// </copyright>
+
+using MediaShop.Common.Models;
+using MediaShop.Common.Models.User;
+
+namespace MediaShop.Common.Dto.User
+{
+    using System;
+
+    /// <summary>
+    /// Class UserDto.
+    /// </summary>
+    /// <seealso cref="MediaShop.Common.Models.Entity" />
+    public class ProfileDto : Entity
+    {
+        /// <summary>
+        /// Gets or sets the date of birth.
+        /// </summary>
+        /// <value>The date of birth.</value>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>The first name.</value>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>The last name.</value>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        /// <value>The phone.</value>
+        public string Phone { get; set; }
+
+    }
+}
