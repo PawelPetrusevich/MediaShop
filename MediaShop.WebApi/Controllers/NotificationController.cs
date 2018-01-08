4fd4130412e61fa3eae7c17b9bca964f4b32265d
Aleksander Shaduro <aleksander_shaduro@epam.com>
2018-01-08 14:33:25 +0300

    * fix build errors * update package

--- MediaShop.WebApi/Controllers/NotificationController.cs
+++ MediaShop.WebApi/Controllers/NotificationController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net;
 using System.Web.Http;
 
 namespace MediaShop.WebApi.Controllers
@@ -12,32 +8,32 @@ namespace MediaShop.WebApi.Controllers
         [HttpGet]
         public IHttpActionResult Get()
         {
-            return this.Ok(new List<Notification>());
+            return this.Ok();
         }
 
         [HttpGet]
         [Route("GetById")]
         public IHttpActionResult Get(ulong id)
         {
-            return this.Ok(new Notification());
+            return this.Ok();
         }
 
         [HttpPost]
         public IHttpActionResult Post([FromBody]ulong userId, [FromBody]string message)
         {
-            return this.Content<Notification>(HttpStatusCode.Created, new Notification());
+            return this.Content(HttpStatusCode.Created, string.Empty);
         }
 
         [HttpPut]
         public IHttpActionResult Put([FromBody]ulong id, [FromBody]string message)
         {
-            return this.Ok(new Notification());
+            return this.Ok();
         }
 
         [HttpDelete]
         public IHttpActionResult Delete([FromBody]ulong id)
         {
-            return this.Ok(new Notification());
+            return this.Ok();
         }
     }
 }
--- MediaShop.WebApi/Controllers/ServiceProductController.cs
+++ MediaShop.WebApi/Controllers/ServiceProductController.cs
@@ -30,6 +30,5 @@ namespace MediaShop.WebApi.Controllers
         {
             return this.Ok();
         }
-
     }
 }
