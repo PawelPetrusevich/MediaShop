5e3c5db36db180e6fa8ad87c0e10cbdbf35c7710
Surmachevsky Evgeny <grek1303@gmail.com>
2018-01-30 20:33:19 +0300

    + Bbrought to class exception filter * Add Exception filter to notification controler

--- MediaShop.WebApi/Areas/Messaging/Controllers/NotificationController.cs
+++ MediaShop.WebApi/Areas/Messaging/Controllers/NotificationController.cs
@@ -5,26 +5,20 @@ using MediaShop.Common.Exceptions;
 using MediaShop.Common.Interfaces.Services;
 using MediaShop.WebApi.Properties;
 using System;
-using System.Net;
 using System.Web.Http;
-using System.Web.Http.Filters;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Net.Http;
 
 namespace MediaShop.WebApi.Areas.Messaging.Controllers
 {
     [RoutePrefix("api/messaging")]
-    public class NotificationController : ApiController, IExceptionFilter
+    [NotificationExceptionFilter]
+    public class NotificationController : ApiController
     {
-        private readonly INotificationService _notificationService;        
+        private readonly INotificationService _notificationService;
 
         public NotificationController(INotificationService notificationService)
         {
             _notificationService = notificationService;
         }
-
-        public bool AllowMultiple => throw new NotImplementedException();
 
         [HttpGet]
         public IHttpActionResult Get(long userId)
@@ -39,7 +33,7 @@ namespace MediaShop.WebApi.Areas.Messaging.Controllers
         }
 
         [HttpPost]
-        public IHttpActionResult CreateNotification([FromBody]NotificationDto notification)
+        public IHttpActionResult CreateNotification([FromBody] NotificationDto notification)
         {
             if (ReferenceEquals(notification, null) || !ModelState.IsValid)
             {
@@ -58,13 +52,6 @@ namespace MediaShop.WebApi.Areas.Messaging.Controllers
             {
                 return this.InternalServerError(ex);
             }
-        }
-
-        public Task ExecuteExceptionFilterAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
-        {
-            actionExecutedContext.Response = actionExecutedContext.Request
-                .CreateErrorResponse(HttpStatusCode.InternalServerError, actionExecutedContext.Exception.Message);
-            return Task.FromResult<object>(null);
         }
     }
 }
