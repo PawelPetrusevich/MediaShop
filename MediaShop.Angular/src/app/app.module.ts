277eede0d718eb963f9433f2d6229457acf8e7ad
Olga Gankovich <olgankov@tut.by>
2018-02-11 17:42:01 +0300

    add component templates and rename classes to dto's

--- MediaShop.Angular/src/app/app.module.ts
+++ MediaShop.Angular/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { SetFlagIsBannedComponent } from './set-flag-is-banned/set-flag-is-banned.component';
+import { SetPermissionComponent } from './set-permission/set-permission.component';
+import { RemovePermissionComponent } from './remove-permission/remove-permission.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterUserComponent,
+    ConfirmComponent,
+    SetFlagIsBannedComponent,
+    SetPermissionComponent,
+    RemovePermissionComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule, NgbModule.forRoot()
---
+++ MediaShop.Angular/src/app/set-permission/set-permission.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-set-permission',
+  templateUrl: './set-permission.component.html',
+  styleUrls: ['./set-permission.component.css']
+})
+export class SetPermissionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
