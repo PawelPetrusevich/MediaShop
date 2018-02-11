ce486ed1d014970aa0e9da1a9fa48e96829e07c5
Alexey Maloed <lyoha.live@gmail.com>
2018-02-11 13:09:59 +0300

    *StopWatchFilterAttribute changed

--- MediaShop.WebApi/Areas/Content/Controllers/Filters/StopWatchFilterAttribute.cs
+++ MediaShop.WebApi/Areas/Content/Controllers/Filters/StopWatchFilterAttribute.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 using System.Web.Mvc;
@@ -20,12 +22,14 @@ namespace MediaShop.WebApi.Areas.Content.Controllers.Filters
         {
             _watch.Reset();
             _watch.Start();
+            Debug.WriteLine($"method name: {actionContext.ActionDescriptor.ActionName}, request: {actionContext.Request}");
         }
 
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             _watch.Stop();
             Debug.WriteLine($"executed: {_watch.ElapsedMilliseconds} ms");
+            Debug.WriteLine($"frguments count: {actionExecutedContext.ActionContext.ActionArguments}, response: {actionExecutedContext.Response}");
         }
     }
 }
