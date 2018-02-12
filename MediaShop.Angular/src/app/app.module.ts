f2eebc3da4191b917b1930a80b419aff705c1acb
Aleksandr Kononenko <kv210@rambler.ru>
2018-02-12 22:29:10 +0300

    + service cart and cartservice in angular

--- MediaShop.Angular/src/app/app.module.ts
+++ MediaShop.Angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
 import { ExecutePaymentComponent } from './execute-payment/execute-payment.component';
+import { ContentCartComponent } from './content-cart/content-cart.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    ExecutePaymentComponent
+    ExecutePaymentComponent,
+    ContentCartComponent
   ],
   imports: [
     BrowserModule, NgbModule.forRoot()
---
+++ MediaShop.Angular/src/app/components/content-cart/content-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-cart',
+  templateUrl: './content-cart.component.html',
+  styleUrls: ['./content-cart.component.css']
+})
+export class ContentCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
