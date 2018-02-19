e4426eeeddb703850a9e570e5db07be40cd95587
Pawel Petrusewitch <shuster.pashka@gmail.com>
2018-02-19 17:26:47 +0300

    *fix error

--- MediaShop.Angular/src/app/app.module.ts
+++ MediaShop.Angular/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { HttpClient } from 'selenium-webdriver/http';
 
 import { AppComponent } from './app.component';
 import { NotificationComponent } from './Messaging/notification/notification.component';
-import { CartComponent } from './cart/cart.component';
-import { ExecutePaymentComponent } from './execute-payment/execute-payment.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ExecutePaymentComponent } from './components/execute-payment/execute-payment.component';
 import { ContentCartComponent } from './components/content-cart/content-cart.component';
@@ -46,7 +44,7 @@ import { ProductInfoComponent } from './components/Content/product-info/product-
     ProductComponent,
     RegisterUserComponent,
     LoginComponent,
-    NotificationComponent
+    NotificationComponent,
     CartComponent,
     ExecutePaymentComponent,
     ContentCartComponent,
