28c954b12744ae52448a4b1d3dcf69f7f6152d3d
Alexey Maloed <lyoha.live@gmail.com>
2018-02-22 16:35:40 +0300

    *fix remarks

--- MediaShop.Angular/src/app/approuting/approuting.module.ts
+++ MediaShop.Angular/src/app/approuting/approuting.module.ts
@@ -16,6 +16,7 @@ import { ConfirmComponent } from '../Components/user/confirm/confirm.component';
 
 import { UserListComponent } from '../components/user/user-list/user-list.component';
 import { SetPermissionComponent } from '../components/user/set-permission/set-permission.component';
+import { ProductDownloadComponent } from '../Components/Content/product-download/product-download.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -48,7 +49,7 @@ import { SetPermissionComponent } from '../components/user/set-permission/set-pe
       },
       {
         path: 'product-download',
-        component: ProductInfoComponent
+        component: ProductDownloadComponent
       },
       {
         path: 'confirm',
