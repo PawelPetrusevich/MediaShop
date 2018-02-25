baa4c8845130d7798cd955732581326003fcb6eb
Surmachevsky Evgeny <grek1303@gmail.com>
2018-02-25 13:51:31 +0300

    + Add origin link to confirmation model + Add origin link to recovery password model * Add api endpoint to environments

--- MediaShop.Angular/src/app/Components/user/confirm/confirm.component.ts
+++ MediaShop.Angular/src/app/Components/user/confirm/confirm.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountService } from '../../../Services/User/AccountService';
 import { HttpErrorResponse } from '@angular/common/http';
 
@@ -15,7 +15,7 @@ export class ConfirmComponent implements OnInit {
   private token: string;
   result: string;
   showError: boolean = false;
-  constructor(private accountService: AccountService, private route: ActivatedRoute) { }
+  constructor(private accountService: AccountService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.route
@@ -27,6 +27,7 @@ export class ConfirmComponent implements OnInit {
     if (this.email && this.token) {
       this.accountService.confirm(this.email, this.token).subscribe(resp => {
         this.result = 'Welcome to Media shop!';
+        this.router.navigate(['login']);
       }, (err: HttpErrorResponse) => {
         this.showError = true;
         this.result = "Oops! There seems to be a problem.";
