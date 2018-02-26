1fd98f8e192d58cb8a65777b155b1b7fb9885ead
Pavel_Ivanenko <pabl1989@mail.ru>
2018-02-26 19:23:00 +0300

    * fix colculate priceAllItem in cart

--- MediaShop.Angular/src/app/components/cart/cart.component.ts
+++ MediaShop.Angular/src/app/components/cart/cart.component.ts
@@ -86,6 +86,8 @@ export class CartComponent implements OnInit {
     }
     this.cartService.deleteById(element.Id).subscribe(resp => {
       if (this.indexCurrentElement > -1) {
+        --this.cart.CountItemsInCollection;
+        this.cart.PriceAllItemsCollection -= this.cart.ContentCartDtoCollection[this.indexCurrentElement].PriceItem;
         this.cart.ContentCartDtoCollection.splice(this.indexCurrentElement, 1);
       } else {
         this.showError = true;
