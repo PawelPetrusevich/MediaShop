d54482f7e3f5674ab891c0c810f56c8da61a945b
Pavel_Ivanenko <pabl1989@mail.ru>
2018-02-26 19:42:09 +0300

    * changed error messaging in cartComponent

--- MediaShop.Angular/src/app/components/cart/cart.component.ts
+++ MediaShop.Angular/src/app/components/cart/cart.component.ts
@@ -8,6 +8,7 @@ import { PaymentComponent } from '../payment/payment.component';
 import { Router } from '@angular/router';
 import { CartDataProvider } from './cartDataProvider';
 import { SignalR } from 'ng2-signalr';
+import { NotificationsService } from 'angular2-notifications';
 
 
 @Component({
@@ -26,7 +27,8 @@ export class CartComponent implements OnInit {
   isPayment = false;
 
   constructor(private cartService: Cartservice, private paymentService: Paymentservice, private router: Router
-    , private dataProvider: CartDataProvider, private _signalR: SignalR) {
+    , private dataProvider: CartDataProvider, private _signalR: SignalR,
+    private notificationService: NotificationsService) {
   }
 
   ngOnInit() {
@@ -35,20 +37,7 @@ export class CartComponent implements OnInit {
       this.dataProvider.storageCart = resp;
       this.isLoaded = true;
     }, (err: HttpErrorResponse) => {
-      this.errorMessage = ' Oops!)) Error....';
-      if (err.status === 404) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 400) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 500) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      this.showError = true;
+      this.ShowError(err);
     }
     );
   }
@@ -58,20 +47,7 @@ export class CartComponent implements OnInit {
       this.cart = resp;
       this.isLoaded = true;
     }, (err: HttpErrorResponse) => {
-      this.errorMessage = ' Oops!)) Error....';
-      if (err.status === 404) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 400) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 500) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      this.showError = true;
+      this.ShowError(err);
     }
     );
   }
@@ -94,20 +70,7 @@ export class CartComponent implements OnInit {
         this.errorMessage = 'element index not found';
       }
     }, (err: HttpErrorResponse) => {
-      this.errorMessage = ' Oops!)) Error....';
-      if (err.status === 404) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 400) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 500) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      this.showError = true;
+      this.ShowError(err);
     }
   );
   }
@@ -118,20 +81,7 @@ export class CartComponent implements OnInit {
       this.cart = resp;
       this.isLoaded = true;
     }, (err: HttpErrorResponse) => {
-      this.errorMessage = ' Oops!)) Error....';
-      if (err.status === 404) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 400) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 500) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      this.showError = true;
+      this.ShowError(err);
     }
   );
   }
@@ -141,20 +91,7 @@ export class CartComponent implements OnInit {
       this.urlForPayment = resp;
       this.paypalExecutePayment();
     }, (err: HttpErrorResponse) => {
-      this.errorMessage = ' Oops!)) Error....';
-      if (err.status === 404) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 400) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      if (err.status === 500) {
-        this.errorMessage = err.statusText;
-        this.showError = true;
-      }
-      this.showError = true;
+      this.ShowError(err);
     }
     );
   }
@@ -163,5 +100,17 @@ export class CartComponent implements OnInit {
     this.dataProvider.storageCart = this.cart;
     this.dataProvider.storageUrl = this.urlForPayment;
     this.router.navigate(['payment']);
+  }
+
+  ShowError(error: HttpErrorResponse) {
+    this.notificationService.error (
+      'OperationError',
+      error.error.Message,
+      {
+        timeOut: 5000,
+        showProgressBar: true,
+        clickToClose: true
+      }
+    );
   }
 }
